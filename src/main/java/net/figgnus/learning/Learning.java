0ea7fd96042d7ebfb00b52830e753315c9ffafe8
Figgnus <109828386+Figgnus@users.noreply.github.com>
2023-11-28 19:17:36 +0100

    Adding Ruby Item Group

--- src/main/java/net/figgnus/learning/Learning.java
+++ src/main/java/net/figgnus/learning/Learning.java
@@ -2,6 +2,7 @@ package net.figgnus.learning;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.figgnus.learning.item.ModItemGroups;
 import net.figgnus.learning.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ public class Learning implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
